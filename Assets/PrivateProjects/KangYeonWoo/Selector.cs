c02dbb352bb9cffc7ba0a7b77be66363d9e260c0
sjk98027 <kakrkddusdn@naver.com>
2016-10-11 14:32:53 +0900

    2016/10/11 테스트용

---
+++ Assets/PrivateProjects/KangYeonWoo/Selector.cs
@@ -0,0 +1,44 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//
+//public class Selector : Node {
+//
+//	//"this class " lack variable so in the variable;
+////	public enum NodeStates{
+////		FAILURE,
+////		SUCCESS,
+////		RUNNING,
+////		FALLURE
+////	};
+//
+//	protected List<Node> m_nodes = new List<Node> ();
+//
+//	public Selector(List<Node> nodes)
+//	{
+//		m_nodes = nodes;
+//	}
+//
+//
+//
+//	public override NodeStates Evealuate(){
+//		foreach (Node node in m_nodes) {
+//			switch (node.Evealuate ()) {
+//			case NodeStates.FAILURE:
+//				continue;
+//			case NodeStates.SUCCESS: 
+//				m_nodeState = NodeStates.SUCCESS;
+//				return m_nodeState;
+//			case NodeStates.RUNNING:
+//				m_nodeState = NodeStates.RUNNING;
+//				return m_nodeState;
+//			default:
+//				continue;
+//			}
+//		}
+//		m_nodeState = NodeStates.FALLURE;
+//		return m_nodeState;
+//
+//	}
+//
+//}
---
+++ Assets/PrivateProjects/KangYeonWoo/TestClass.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestClass : MonoBehaviour {
+
+	bool state;
+	int type;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		type = Random.Range (0, 3);
+
+//		state = Pattern1 ();
+	}
+
+	public bool Pattern1 (int _condition, int _hp, int _sp, int _action){
+		return state;
+	}
+
+//	public bool pattern2 (int _condition){
+//	}
+
+//	public bool pattern3(int _condition){
+//	}
+}
